da098580e73788c3b47903087260410e781250cb
Hironari Iwata <hiro_wrty1028_java@yahoo.co.jp>
2018-05-05 19:59:30 +0900

    modify api params

--- app.py
+++ app.py
@@ -9,26 +9,25 @@ app.config['JSON_AS_ASCII'] = False
 def hello():
     return "kawasaki-trash-api"
 
-@app.route('/api/v1/throw_rule', methods=['GET'])
-def throw_rule():
+@app.route('/api/v1/trashes/<trash_name>/categories', methods=['GET'])
+def throw_rule(trash_name):
     response = dict()
-    if 'trash_name' in request.args:
-        try:
-            trash = db_session.query(Trash).filter(Trash.name==request.args['trash_name']).first()
-            categories = []
-            for rule in trash.throw_rules:
-                tmp = dict()
-                tmp['id'] = rule.category.category_id
-                tmp['name'] = rule.category.name
-                categories.append(tmp)
-            rule = dict()
-            rule['categories'] = categories
-            rule['detail'] = trash.detail
-            response["rule"] = [rule]
-            response["code"] = 200
-        except:
-            response = error_message(400)
-    else:
+    try:
+        print(trash_name)
+        trash = db_session.query(Trash).filter(Trash.name==trash_name).first()
+        categories = []
+        for rule in trash.throw_rules:
+            tmp = dict()
+            tmp['id'] = rule.category.category_id
+            tmp['name'] = rule.category.name
+            categories.append(tmp)
+        
+        rule = dict()
+        rule['categories'] = categories
+        rule['detail'] = trash.detail
+        response["rule"] = [rule]
+        response["code"] = 200
+    except:
         response = error_message(400)
     return jsonify(response)
 
