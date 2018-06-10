73f405e5c46fb566762e51448c081b94ede924a2
Hironari Iwata <hiro_wrty1028_java@yahoo.co.jp>
2018-06-10 22:42:04 +0900

    fix func_dump

--- do_dump.py
+++ do_dump.py
@@ -7,6 +7,14 @@ BASEPATH = './data/backup/'
 
 data = dict()
 
+# datetimeの変換関数
+def json_serial(obj):
+    # 日付型の場合には、文字列に変換します
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    # 上記以外はサポート対象外.
+    raise TypeError ("Type %s not serializable" % type(obj))
+
 def convert_rec2dic(records):
     arr = []
     for record in records:
@@ -36,4 +44,4 @@ time = datetime.now().strftime("%Y%m%d%H%M%S")
 output_path = BASEPATH + time + '.json'
 
 with open(output_path, 'w') as f:
-    json.dump(data, f, indent=2, sort_keys=False, ensure_ascii=False)
+    json.dump(data, f, indent=2, sort_keys=False, ensure_ascii=False, default=json_serial)
